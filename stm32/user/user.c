9570b644a54a2d10e73ef49330e137d4e88ebcc9
Liao MY <liaomaoyi@126.com>
2016-08-10 11:45:13 +0800

    转换到欧拉角

--- stm32/user/user.c
+++ stm32/user/user.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <math.h>
 #include "stm32f1xx_hal.h"
 #include "user.h"
 #include "led.h"
@@ -26,12 +27,22 @@ void user_init2(){
 
 void user_loop(){
 	long gyro[3],accel[3],quat[4];
+	float w,x,y,z;
 	unsigned long timestamp;
 	short sensors;
 	char more;
 	dmp_read_fifo(gyro, accel, quat, &timestamp, &sensors, &more);
 	//printf( "sensors=%d\r\n", sensors );
 	if( sensors & INV_WXYZ_QUAT ){
-		printf( "%d,%d,%d,%d\r\n", quat[0],quat[1],quat[2],quat[3] );
+		//printf( "%d,%d,%d,%d\r\n", quat[0],quat[1],quat[2],quat[3] );
+		//http://www.cnblogs.com/wqj1212/archive/2010/11/21/1883033.html
+		w = quat[0]/1073741824.0f;
+		x = quat[1]/1073741824.0f;
+		y = quat[2]/1073741824.0f;
+		z = quat[3]/1073741824.0f;
+
+		float a = atan2( 2*(w*x+yz),1-2*(x*x+y*y) );
+		float b = asin( 2*(w*y-z*x) );
+		float c = atan2( 2*(w*z+x*y),1-2*(y*y+z*z) );
 	}
 }
